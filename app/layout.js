80a71d0b2dd92bfb61f14695763565e1a0879556
Kate Bark <barkkate@gmail.com>
2025-05-22 14:50:31 -0400

    set up favicon, metadata in layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,17 @@
 import "@/app/_styles/globals.css";
+
+export const metadata = {
+  title: {
+    template: "%s Emerson Fyne",
+    default: "Home of Emerson Fyne",
+  },
+  description: "Digital home of composer, singer, audio engineer Emerson Fyne)",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
