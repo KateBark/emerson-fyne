17b7311b83942c406fb42b5f35cf0d09a9c6fdbd
Kate Bark <barkkate@gmail.com>
2025-07-10 17:56:26 -0400

    writing-page code cleanup

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -28,31 +28,3 @@ export async function getFeatureVideo(id) {
 
   return data || [];
 }
-
-export async function getLyrics(ids) {
-  const { data, error } = await supabase
-    .from("writings")
-    .select("*")
-    .in("id", ids);
-
-  if (error) {
-    console.log(error);
-    throw new Error("Lyrics could not be loaded");
-  }
-  return data || [];
-}
-
-export async function getFeatureLyrics(id) {
-  const { data, error } = await supabase
-    .from("writings")
-    .select("*")
-    .eq("id", id)
-    .single();
-
-  if (error) {
-    console.error(error);
-    throw new Error("Lyrics could not be loaded");
-  }
-
-  return data || [];
-}
--- app/writing/page.js
+++ app/writing/page.js
@@ -1,20 +1,14 @@
 import supabase from "@/app/_lib/supabase";
 import WritingList from "@/app/_components/WritingList";
-
 import Image from "next/image";
 import tunnel from "@/public/forest-tunnel.jpg";
 import { germinal } from "../fonts/germinal";
-import LyricsLink from "../_components/LyricsLink";
-
-import { getLyrics } from "../_lib/data-service";
-import { getFeatureLyrics } from "../_lib/data-service";
 
 export const metadata = {
   title: "Writing |",
 };
 
 export default async function Page() {
-  // const supabase = createClient();
   const { data: writings } = await supabase
     .from("writings")
     .select("*")
@@ -41,39 +35,3 @@ export default async function Page() {
     </main>
   );
 }
-
-// export default async function Page() {
-//   const lyrics = await getLyrics([1, 2, 3]);
-//   const featureLyrics = await getFeatureLyrics(4);
-
-//   return (
-//     <div>
-//       <h1 className="text-5xl text-center pb-10">W R I T I N G</h1>
-//       <div
-//         className={`${germinal.className} flex text-6xl justify-center pb-10`}
-//       >
-//         <h2 className="pr-20">l y r i c s</h2>
-//       </div>
-
-//       <div className="grid grid-cols-4 gap-x-24 gap-y-32 items center pt-12">
-//         <div className="col-span-2">
-//           <Image src={tunnel} alt="running through fields" />
-//         </div>
-//         <div className="col-span-2">
-//           <h2 className="text-center text-lg">Featured:</h2>
-
-// //           {featureLyrics && <LyricsLink lyrics={featureLyrics} />}
-// //         </div>
-// //       </div>
-//       <div>
-//         {lyrics.length > 0 && (
-//           <div className="m-40">
-//             {lyrics.map((lyrics) => (
-//               <LyricsLink key={lyrics.id} lyrics={lyrics} />
-//             ))}
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
