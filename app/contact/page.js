132b1e1bb4aee3168fc4e49bb4515693bc1877fc
Kate Bark <barkkate@gmail.com>
2025-12-03 10:46:36 -0500

    made contact page responsive

--- app/contact/page.js
+++ app/contact/page.js
@@ -8,18 +8,24 @@ export const metadata = {
 
 export default function Page() {
   return (
-    <div>
-      <h1 className="text-5xl text-center pb-10">C O N T A C T</h1>
+    <div className="px-4 md:px-0">
+      <h1 className="md:text-5xl text-center pb-10">C O N T A C T</h1>
 
-      <div className="grid grid-cols-4 gap-x-24 gap-y-32 items center pt-12">
-        <div className="col-span-2">
-          <Image src={tower} alt="running through fields" />
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-x-6 md:gap-x-24 gap-y-12 md:gap-y-32 items-center pt-12">
+        <div className="col-span-1 md:col-span-2">
+          <div className="w-full">
+            <Image
+              src={tower}
+              alt="tower"
+              className="w-full h-auto object-cover rounded"
+            />
+          </div>
         </div>
-        <div className="col-span-2 mt-20">
+        <div className="col-span-1 md:col-span-2 mt-8 md:mt-20">
           <p className="mb-5">For sync licensing & other inquiries:</p>
           <div>
             <a
-              className="hover:text-accent-300"
+              className="hover:text-accent-300 break-words"
               href="mailto:emerson@emersonfyne.com"
             >
               emerson@emersonfyne.com
