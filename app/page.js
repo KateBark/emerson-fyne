afc07b2693db1f3c31d3447cbab070f69825f92f
Kate Bark <barkkate@gmail.com>
2025-12-03 10:52:57 -0500

    made landing page responsive

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import bg from "@/public/bg-forest.jpg";
 
 export default function Page() {
   return (
-    <main className="mt-24">
+    <main className="mt-12 md:mt-24">
       <Image
         src={bg}
         fill
@@ -14,9 +14,11 @@ export default function Page() {
         className="object-cover object-top"
         alt="forest"
       />
-      <div className="relative z-10 text-white text-center">
-        <h1 className={`${germinal.className} text-8xl`}>Emerson~fyne</h1>
-        <div className="flex justify-around mt-20 text-4xl">
+      <div className="relative z-10 text-white text-center px-4">
+        <h1 className={`${germinal.className} text-4xl md:text-8xl`}>
+          Emerson~fyne
+        </h1>
+        <div className="flex flex-col md:flex-row justify-around gap-6 md:gap-0 mt-10 md:mt-20 text-2xl md:text-4xl">
           <Link href="/music" className="hover:text-accent-400">
             Music
           </Link>
